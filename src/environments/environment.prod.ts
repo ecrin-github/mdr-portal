d2b328b2e7cf43dee0f2d031f348a3f63c4b3c94
Sergei Gorianin <frequenteen@gmail.com>
2021-08-27 18:07:38 +0200

    hot fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,10 @@
 export const environment = {
   production: true,
-  appVersion: 'v.0.6.3',
+  appVersion: 'v.0.6.4',
 
   // HOSTNAME
-  hostname: 'https://localhost:5001/',
+  // hostname: 'https://localhost:5001/',
+  hostname: 'https://api.crmdr.org/',
   // hostname: 'http://51.210.99.18:8001/',
 
   // Back-end configs
