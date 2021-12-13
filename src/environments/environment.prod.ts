8d4143b35578ee7ae2374bf5e9a3d1249a5362a5
Sergei Gorianin <frequenteen@gmail.com>
2021-12-13 13:51:00 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,15 +3,16 @@ export const environment = {
   appVersion: 'v.0.7.2',
 
   // HOSTNAME
-  hostname: 'http://localhost:5100/',
-  // hostname: 'https://api.ecrin-rms.org/',
+  // hostname: 'http://localhost:5100/',
+  hostname: 'https://api.ecrin-rms.org/',
 
   // Back-end configs
   queryBaseUrl: 'api/v1/search',
   rawQueryBaseUrl: 'api/v1/raw-sql-search',
-  esQueryBaseUrl: 'api/v1/es',
+  // esQueryBaseUrl: 'api/v1/es',
   // queryBaseUrl: 'mdr/api/v1/search',
   // rawQueryBaseUrl: 'mdr/api/v1/raw-sql-search',
+  esQueryBaseUrl: 'mdr/api/v1/es',
 
   // API Query URLs
   specificStudyUrl: '/specific-study',
