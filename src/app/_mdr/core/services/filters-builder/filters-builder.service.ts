e4876be58a1078a2c8642c7fd4733dc43a7bbfa0
Sergei Gorianin <frequenteen@gmail.com>
2021-12-15 09:06:41 +0100

    Update filters-builder.service.ts

--- src/app/_mdr/core/services/filters-builder/filters-builder.service.ts
+++ src/app/_mdr/core/services/filters-builder/filters-builder.service.ts
@@ -3,11 +3,22 @@ import {FiltersRequestInterface} from '../../interfaces/filters/filters.interfac
 import {StatesService} from '../state/states.service';
 
 
+interface EsFilters {
+  studyTypes: number[];
+  studyStatuses: number[];
+  studyGenderEligs: number[];
+  studyFeatures: number[];
+  objectTypes: number[];
+  objectAccessTypes: number[];
+}
+
+
 @Injectable({providedIn: 'root'})
 export class FiltersBuilderService {
 
   exceptionDbFiltersArray: FiltersRequestInterface;
-  exceptionEsFiltersArray: any[];
+  exceptionEsFilters: EsFilters;
+  exceptionEsFiltersArray: any[] = [];
 
   STUDY_FEATURE_PARAMS: string[] = ['Phase', 'Intervention model',
     'Allocation type', 'Primary purpose', 'Masking', 'Observational model',
@@ -65,36 +76,164 @@ export class FiltersBuilderService {
   }
 
 
-  elasticsearchFiltersBuilder(): any[] {
-
-    this.exceptionEsFiltersArray = [];
+  elasticsearchFiltersIdsBuilder(): EsFilters {
+    this.exceptionEsFilters = {
+      studyTypes: [],
+      studyStatuses: [],
+      studyGenderEligs: [],
+      studyFeatures: [],
+      objectTypes: [],
+      objectAccessTypes: [],
+    };
 
     if (this.statesService.filtersList !== null && this.statesService.filtersList !== undefined) {
       if (this.statesService.filtersList.length > 0) {
         for (const filter of this.statesService.filtersList) {
-          if (filter.isNested) {
-            const fieldName = filter.fieldName;
-            const filterOption = {
-              nested: {
-                path: filter.path,
-                query: {}
-              }
-            };
-            const termFilter = {};
-            termFilter[fieldName] = filter.value;
-            filterOption.nested.query['term'] = termFilter;
-            this.exceptionEsFiltersArray.push(filterOption);
-          } else {
-            const filterOption = {
-              term: {}
-            };
-            filterOption.term[filter.fieldName] = filter.value;
-            this.exceptionEsFiltersArray.push(filterOption);
+
+          if (filter.fieldName === 'study_type.id') {
+            if (!this.exceptionEsFilters.studyTypes.includes(filter.value)) {
+              this.exceptionEsFilters.studyTypes.push(filter.value);
+            }
+          }
+
+          if (filter.fieldName === 'study_status.id') {
+            if (!this.exceptionEsFilters.studyStatuses.includes(filter.value)) {
+              this.exceptionEsFilters.studyStatuses.push(filter.value);
+            }
+          }
+
+          if (filter.fieldName === 'study_gender_elig.id') {
+            if (!this.exceptionEsFilters.studyGenderEligs.includes(filter.value)) {
+              this.exceptionEsFilters.studyGenderEligs.push(filter.value);
+            }
+          }
+
+          if (filter.fieldName === 'study_features.feature_value.id') {
+            if (!this.exceptionEsFilters.studyFeatures.includes(filter.value)) {
+              this.exceptionEsFilters.studyFeatures.push(filter.value);
+            }
+          }
+
+          if (filter.fieldName === 'linked_data_objects.object_type.id') {
+            if (!this.exceptionEsFilters.objectTypes.includes(filter.value)) {
+              this.exceptionEsFilters.objectTypes.push(filter.value);
+            }
+          }
+
+          if (filter.fieldName === 'linked_data_objects.access_type.id') {
+            if (!this.exceptionEsFilters.objectAccessTypes.includes(filter.value)) {
+              this.exceptionEsFilters.objectAccessTypes.push(filter.value);
+            }
           }
         }
       }
-      return this.exceptionEsFiltersArray;
     }
+    return this.exceptionEsFilters;
+  }
+
+
+
+  elasticsearchFiltersBuilder(): any[] {
+
+    this.exceptionEsFiltersArray = [];
+
+    this.exceptionEsFilters = this.elasticsearchFiltersIdsBuilder();
+
+    if (this.exceptionEsFilters.studyTypes.length > 0){
+      const filterOption = {
+        terms: {}
+      };
+      filterOption.terms['study_type.id'] = this.exceptionEsFilters.studyTypes;
+      this.exceptionEsFiltersArray.push(filterOption);
+    }
+
+    if (this.exceptionEsFilters.studyStatuses.length > 0){
+      const filterOption = {
+        terms: {}
+      };
+      filterOption.terms['study_status.id'] = this.exceptionEsFilters.studyStatuses;
+      this.exceptionEsFiltersArray.push(filterOption);
+    }
+
+    if (this.exceptionEsFilters.studyGenderEligs.length > 0){
+      const filterOption = {
+        terms: {}
+      };
+      filterOption.terms['study_gender_elig.id'] = this.exceptionEsFilters.studyGenderEligs;
+      this.exceptionEsFiltersArray.push(filterOption);
+    }
+
+    if (this.exceptionEsFilters.studyFeatures.length > 0){
+      const filterOption = {
+        nested: {
+          path: 'study_features',
+          query: {}
+        }
+      };
+      filterOption.nested.query['terms']['study_features.feature_value.id'] = this.exceptionEsFilters.studyGenderEligs;
+      this.exceptionEsFiltersArray.push(filterOption);
+    }
+
+    if (this.exceptionEsFilters.objectTypes.length > 0 || this.exceptionEsFilters.objectAccessTypes.length > 0){
+      if (this.exceptionEsFilters.objectTypes.length > 0 && !(this.exceptionEsFilters.objectAccessTypes.length > 0)){
+        const filterOption = {
+          nested: {
+            path: 'linked_data_objects',
+            inner_hits: {
+              size: 100,
+              from: 0
+            },
+            query: {}
+          }
+        };
+        const termFilter = {};
+        termFilter['linked_data_objects.object_type.id'] = this.exceptionEsFilters.objectTypes;
+        filterOption.nested.query['terms'] = termFilter;
+        this.exceptionEsFiltersArray.push(filterOption);
+      } else if (!(this.exceptionEsFilters.objectTypes.length > 0) && this.exceptionEsFilters.objectAccessTypes.length > 0){
+        const filterOption = {
+          nested: {
+            path: 'linked_data_objects',
+            inner_hits: {
+              size: 100,
+              from: 0
+            },
+            query: {}
+          }
+        };
+        const termFilter = {};
+        termFilter['linked_data_objects.access_type.id'] = this.exceptionEsFilters.objectAccessTypes;
+        filterOption.nested.query['terms'] = termFilter;
+        this.exceptionEsFiltersArray.push(filterOption);
+      } else {
+        const otFilterOption = {
+          nested: {
+            path: 'linked_data_objects',
+            inner_hits: {
+              size: 100,
+              from: 0
+            },
+            query: {}
+          }
+        };
+        const otTermFilter = {};
+        otTermFilter['linked_data_objects.object_type.id'] = this.exceptionEsFilters.objectTypes;
+        otFilterOption.nested.query['terms'] = otTermFilter;
+        this.exceptionEsFiltersArray.push(otFilterOption);
+
+        const filterOption = {
+          nested: {
+            path: 'linked_data_objects',
+            query: {}
+          }
+        };
+        const termFilter = {};
+        termFilter['linked_data_objects.access_type.id'] = this.exceptionEsFilters.objectAccessTypes;
+        filterOption.nested.query['terms'] = termFilter;
+        this.exceptionEsFiltersArray.push(filterOption);
+      }
+    }
+    return this.exceptionEsFiltersArray;
   }
 }
 
